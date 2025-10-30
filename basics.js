0cc40257f6b5f90dcc1d06d22b1dddb544a4d0d8
phoenixx-png <jojoeydenver@gmail.com>
2025-10-30 14:20:45 +0200

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,15 @@
+const firstName = "Phoenix";
+const lastName = "Chung";
+
+var age = 20;
+
+let fullName = firstName + " " + lastName;
+
+console.log("Hello, my name is", firstName)
+console.log("My surname is", lastName)
+console.log("I am", age, "years old")
+
+age += 2
+console.log("In two years, I will be", age, "years old")
+
+console.log("My full name is", fullName)
